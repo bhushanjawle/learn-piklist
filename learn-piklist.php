a6c9f4f2755a464f57540f5fb2752049d974ae66
Bhushan Jawle <bhushan@sanganaktechnologies.com>
2017-10-23 16:57:04 +0530

    Initial Commit

---
+++ learn-piklist.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Learn Piklist
+Plugin URI: https://www.sanganaktechnologies.com
+Description: Learn Piklist with examples 
+Version: 1.0
+Author: Bhushan S. Jawle
+Author URI: https://www.sanganaktechnologies.com
+Text Domain: learn-piklist
+License: GPL2
+*/
